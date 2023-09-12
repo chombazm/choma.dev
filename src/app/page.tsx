dab62a5c15bf9624f10351ef813153ef63891ab1
Chomba <dev.chomba@gmail.com>
2023-09-12 07:29:33 +0200

    added interests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between lg:px-40">
       <div className="z-10 w-full items-center justify-between font-mono text-sm flex fixed">
-        <p className="flex justify-center lg:pl-40 dark:bg-zinc-800/30 dark:from-inherit lg:static w-auto  lg:bg-gray-200 py-4  lg:dark:bg-zinc-800/30">
+        <p className="flex justify-center lg:pl-40 dark:bg-zinc-800/30 dark:from-inherit lg:static w-auto  py-4  lg:dark:bg-zinc-800/30">
           @dev.chomba 🇿🇲
         </p>
         <div className="  items-end justify-center  from-white via-white dark:from-black dark:via-black static h-auto w-auto bg-none">
@@ -42,7 +42,7 @@ export default function Home() {
           My name is <span className="font-bold">Chomba</span>, <br /> A
           software engineer. <br />
         </h1>
-        <p className="text-1xl mt-10 leading-[2]">
+        <p className="text-1xl mt-10 leading-[2] pb-4">
           Currently working with great minds and building viable solutions that{" "}
           <br />
           change the world at{" "}
@@ -92,6 +92,32 @@ export default function Home() {
             Kezo
           </a>
           ,
+        </p>
+
+        <p>
+          I listen to a lot of{" "}
+          <a href="https://open.spotify.com/user/s363ssbbtu0jz6zuqad6xof8g">
+            music
+          </a>{" "}
+          and{" "}
+          <a className="font-bold underline" href="https://syntax.fm/">
+            podcasts
+          </a>{" "}
+          and I love{" "}
+          <a
+            className="font-bold underline"
+            href="https://www.instagram.com/dev.chomba/"
+          >
+            DIY
+          </a>{" "}
+          and{" "}
+          <a
+            className="font-bold underline"
+            href="https://www.instagram.com/dev.chomba/"
+          >
+            take photos
+          </a>
+          .
         </p>
         {/* <span className="font-bold text-xs">
           Read more about me <a href="/about">here</a>.
